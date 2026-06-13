a3215c92539d06fff1181632d55a9adfb2011034
Miguel Basaglia <miguelbasaglia08@gmail.com>
2026-06-13 10:02:39 -0300

    Atividade da COMPETEC / Variavel 2

---
+++ aula-1/variavel2.java
@@ -0,0 +1,12 @@
+public class Main 
+{
+    public static void main(String[] args) {
+        int valor1 = 1;
+        int valor2 = 2;
+        int valor3 = 3;
+
+        int soma = valor1 + valor2 + valor3;
+
+        System.out.println(soma);
+    }
+}
