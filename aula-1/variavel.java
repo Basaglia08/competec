29f2f72d712e16bc4557041e775307a0ee3419ae
Miguel Basaglia <miguelbasaglia08@gmail.com>
2026-06-13 10:01:46 -0300

    Atividade da COMPETEC / Variavel 1

---
+++ aula-1/variavel.java
@@ -0,0 +1,10 @@
+public class Main 
+{
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Posição " + i + ": " + numeros[i]);
+        }
+    }
+}
